3869ca054e68a6744906394be6d4e0eb0f6988db
Danylo <golakdanilo@gmail.com>
2026-01-01 22:00:21 +0200

    try6

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -3,11 +3,11 @@ package core.basesyntax;
 public class UserService {
     private PasswordValidator passwordValidator;
 
-    public void registerUser(User user) throws PasswordValidationException {
+    public void registerUser(User user) {
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
         } catch (PasswordValidationException e) {
-            throw new PasswordValidationException("Your passwords are incorrect. Try again.");
+            System.out.println("Your passwords are incorrect. Try again.");
         }
         saveUser(user);
     }
